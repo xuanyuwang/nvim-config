259a4b97076a41ab402df70b4aa986afbbaf9230
Xuanyu Wang <xuanyu.wang@outlook.com>
2024-04-20 23:01:37 -0400

    Add telescope shortcuts

--- lua/plugins/cauppuccin.lua
+++ lua/plugins/cauppuccin.lua
@@ -4,6 +4,17 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+        require("catppuccin").setup({
+            integrations = {
+                gitsigns = true,
+                neotree = true,
+                telescope = {
+                    enabled = true,
+                },
+                treesitter = true,
+                mason = true,
+            }
+        })
 		vim.cmd.colorscheme "catppuccin"
 	end
 }
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,9 @@ return {
       dependencies = { 'nvim-lua/plenary.nvim' },
       config = function()
 	      local builtin = require("telescope.builtin")
-	      vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-	      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+	      vim.keymap.set("n", "<C-p>", builtin.find_files, {desc = "Find files"})
+	      vim.keymap.set("n", "<leader>fs", builtin.live_grep, {desc = "Search in cwd"})
+	      vim.keymap.set("n", "<leader>ft", builtin.colorscheme, {desc = "Colorscheme"})
+	      vim.keymap.set("n", "<leader>gs", builtin.git_status, {desc = "Git status"})
       end
     }
