25eee7a2eceee5b2514063d24951204692020ce8
Xuanyu Wang <xuanyu.wang@outlook.com>
2024-04-21 10:58:13 -0400

    Add formatters and formatted all files

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,12 +1,31 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-      config = function()
-	      local builtin = require("telescope.builtin")
-	      vim.keymap.set("n", "<C-p>", builtin.find_files, {desc = "Find files"})
-	      vim.keymap.set("n", "<leader>fs", builtin.live_grep, {desc = "Search in cwd"})
-	      vim.keymap.set("n", "<leader>ft", builtin.colorscheme, {desc = "Colorscheme"})
-	      vim.keymap.set("n", "<leader>gs", builtin.git_status, {desc = "Git status"})
-      end
-    }
+	{
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.6",
+		-- or                              , branch = '0.1.x',
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<C-p>", builtin.find_files, { desc = "Find files" })
+			vim.keymap.set("n", "<leader>fs", builtin.live_grep, { desc = "Search in cwd" })
+			vim.keymap.set("n", "<leader>ft", builtin.colorscheme, { desc = "Colorscheme" })
+			vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
+		end,
+	},
+	{
+		"nvim-telescope/telescope-ui-select.nvim",
+		config = function()
+			-- This is your opts table
+			require("telescope").setup({
+				extensions = {
+					["ui-select"] = {
+						require("telescope.themes").get_dropdown({}),
+					},
+				},
+			})
+			-- To get ui-select loaded and working with telescope, you need to call
+			-- load_extension, somewhere after setup function:
+			require("telescope").load_extension("ui-select")
+		end,
+	},
+}
