6cae0227fe09d4b1efe287d9e686cfc5966c9478
Xuanyu Wang <xuanyu.wang@outlook.com>
2024-04-23 07:46:42 -0400

    Set line number of add diagnostic window

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,34 +1,48 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"gopls",
-				},
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = {
+                    "lua_ls",
+                    "gopls",
+                },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
         keys = {
-            {"K", vim.lsp.buf.hover, mode = "n", desc = "LSP: Definition"},
-            {"gd", vim.lsp.buf.definition, mode = {"n", "v"}, desc = "LSP: Definition"},
-            {"<leader>ca", vim.lsp.buf.code_action, mode = {"n", "v"}, desc = "LSP: Code actions"},
-            {"<leader>rn", vim.lsp.buf.rename, mode = {"n"}, desc = "LSP: Rename variable"},
+            { "K",          vim.lsp.buf.hover,       mode = "n",        desc = "LSP: Definition" },
+            { "gd",         vim.lsp.buf.definition,  mode = { "n", "v" }, desc = "LSP: Definition" },
+            { "<leader>ca", vim.lsp.buf.code_action, mode = { "n", "v" }, desc = "LSP: Code actions" },
+            { "<leader>rn", vim.lsp.buf.rename,      mode = { "n" },    desc = "LSP: Rename variable" },
         },
-		config = function()
-			local lspconfig = require("lspconfig")
+        config = function()
+            local lspconfig = require("lspconfig")
 
-			lspconfig.lua_ls.setup({})
+            lspconfig.lua_ls.setup({})
             lspconfig.gopls.setup({})
-		end,
-	},
+        end,
+    },
+    {
+        "folke/trouble.nvim",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons"
+        },
+        keys = {
+            {"<leader>q", "<CMD>TroubleToggle<CR>", mode = "n", desc = "Diagnostics: Toggle"}
+        },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    }
 }
