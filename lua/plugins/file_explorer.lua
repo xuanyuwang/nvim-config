18ddcd393b05e7183a43246ed0d336e9d6dc1d35
Xuanyu Wang <xuanyu.wang@cresta.ai>
2024-04-24 22:38:21 -0400

    Resize explorer automatically

--- lua/plugins/file_explorer.lua
+++ lua/plugins/file_explorer.lua
@@ -1,34 +1,36 @@
 return {
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		keys = {
-			{ "<C-n>", "<cmd>NvimTreeFindFileToggle<cr>", mode = "n", desc = "File explorer: Toggle and focus" },
-		},
-		config = function()
-			local function my_on_attach(bufnr)
-				local api = require("nvim-tree.api")
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        keys = {
+            { "<C-n>", "<cmd>NvimTreeFindFileToggle<cr>", mode = "n", desc = "File explorer: Toggle and focus" },
+        },
+        config = function()
+            local function my_on_attach(bufnr)
+                local api = require "nvim-tree.api"
 
-				-- local function opts(desc)
-				-- 	return { desc = "File explorer: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-				-- end
+                local function opts(desc)
+                    return { desc = "Explorer: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+                end
 
-				-- default mappings
-				api.config.mappings.default_on_attach(bufnr)
+                -- default mappings
+                api.config.mappings.default_on_attach(bufnr)
 
-				-- vim.keymap.set("n", "<C-n>", api.tree.toggle, opts("Open and focus"))
-				-- vim.keymap.set("n", "?", api.tree.toggle_help, opts("Help"))
-			end
-			-- pass to setup along with your other options
-			require("nvim-tree").setup({
-				---
-				on_attach = my_on_attach,
-				---
-			})
-		end,
-	},
+                -- custom mappings
+                vim.keymap.set('n', '/', api.live_filter.start, opts('Filter'))
+            end
+            require("nvim-tree").setup({
+                on_attach = my_on_attach,
+                view = {
+                    width = {
+                        max = -1,
+                    }
+                }
+            })
+        end,
+    },
 }
