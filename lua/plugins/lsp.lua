407cecc3114b606689c3f83c504e23f162bee542
Xuanyu Wang <xuanyu.wang@cresta.ai>
2024-05-06 11:27:11 -0400

    Upgrade autocomplete

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -19,30 +19,37 @@ return {
     {
         "neovim/nvim-lspconfig",
         keys = {
-            { "K",          vim.lsp.buf.hover,       mode = "n",        desc = "LSP: Definition" },
+            { "K",          vim.lsp.buf.hover,       mode = "n",          desc = "LSP: Definition" },
             { "gd",         vim.lsp.buf.definition,  mode = { "n", "v" }, desc = "LSP: Definition" },
             { "<leader>ca", vim.lsp.buf.code_action, mode = { "n", "v" }, desc = "LSP: Code actions" },
-            { "<leader>rn", vim.lsp.buf.rename,      mode = { "n" },    desc = "LSP: Rename variable" },
+            { "<leader>rn", vim.lsp.buf.rename,      mode = { "n" },      desc = "LSP: Rename variable" },
         },
         config = function()
             local lspconfig = require("lspconfig")
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+            local servers = { "lua_ls", "gopls" }
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.gopls.setup({})
+            for _, server in ipairs(servers) do
+                lspconfig[server].setup({
+                    capabilities = capabilities,
+                })
+            end
         end,
     },
     {
         "folke/trouble.nvim",
         dependencies = {
-            "nvim-tree/nvim-web-devicons"
+            "nvim-tree/nvim-web-devicons",
         },
         keys = {
-            {"<leader>q", "<CMD>TroubleToggle<CR>", mode = "n", desc = "Diagnostics: Toggle"}
+            { "<leader>qq", "<CMD>TroubleToggle document_diagnostics<CR>", mode = "n", desc = "Diagnostics: Toggle for current document" },
+            { "<leader>qw", "<CMD>TroubleToggle workspace_diagnostics<CR>", mode = "n", desc = "Diagnostics: Toggle for worksapce" },
+            { "<leader>qf", "<CMD>TroubleToggle quickfix<CR>", mode = "n", desc = "Diagnostics: Toggle for quickfix" },
         },
         opts = {
             -- your configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
         },
-    }
+    },
 }
